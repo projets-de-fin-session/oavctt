50b4bce27879a360ac548b1cfe34daa11003048e
valfranceau <pirahstore@yahoo.com>
2021-03-18 10:26:05 -0400

    new pull

--- app/src/main/java/com/example/animsplashdemo/Services.java
+++ app/src/main/java/com/example/animsplashdemo/Services.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class Services extends AppCompatActivity {
     private ViewFlipper flipper;
     private ImageView image;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class Services extends AppCompatActivity {
         int images[] = {R.drawable.vc, R.drawable.vct, R.drawable.image, R.drawable.oavct_tabarre_2};
         flipper = (ViewFlipper) findViewById(R.id.viewFliper);
         image = (ImageView) findViewById(R.id.back);
+        btn1= findViewById ( R.id.bouton1 );
 
         for (int image: images){
             flipers(image);
@@ -32,6 +35,16 @@ public class Services extends AppCompatActivity {
             public void onClick(View v) {
                 onBackPressed();
                 Intent intent = new Intent(Services.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                Intent intent = new Intent(Services.this, VehiculeInsurance.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/animsplashdemo/VehiculeInsurance.java
+++ app/src/main/java/com/example/animsplashdemo/VehiculeInsurance.java
@@ -1,29 +1,85 @@
 package com.example.animsplashdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.File;
+//import com.itextpdf.layout.Document;
+//import com.itextpdf.kernel.pdf.PdfWriter;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.lang.annotation.Documented;
 
 public class VehiculeInsurance extends AppCompatActivity {
-    private ImageView image;
 
+
+    private EditText editText;
+    private EditText noassurance;
+    private EditText vehicule;
+    private EditText noassurances;
+    private EditText noplaque;
+    private  EditText annedue;
+    private Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vehicule_insurance);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_vehicule_insurance );
 
-        image = (ImageView) findViewById(R.id.back);
+        editText= findViewById ( R.id.editTextName );
+        noassurance= findViewById ( R.id.noassurance );
+        vehicule= findViewById ( R.id.vehicule);
+        noassurance= findViewById ( R.id.noassurance );
+        noplaque= findViewById ( R.id.noplaque );
+        annedue= findViewById ( R.id.anneDue);
+        submitButton = findViewById (R.id.button);
 
-        image.setOnClickListener(new View.OnClickListener() {
+        ActivityCompat.requestPermissions ( VehiculeInsurance.this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED );
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
-                Intent intent = new Intent(VehiculeInsurance.this, Home.class);
-                startActivity(intent);
+
             }
         });
     }
+
+
+public void createMyPDF(View view){
+        PdfDocument myPdfDocument= new PdfDocument ();
+        PdfDocument.PageInfo myPageInfo= new PdfDocument.PageInfo.Builder ( 300,600,1 ).create ();
+        PdfDocument.Page myPage= myPdfDocument.startPage (myPageInfo);
+
+        Paint myPaint =new Paint ();
+        String myString = editText.getText ().toString();
+        int x= 10 , y=25;
+        myPage.getCanvas().drawText (myString,x,y,myPaint);
+        myPdfDocument.finishPage (myPage);
+        String myFilePath= Environment.getExternalStorageDirectory().getPath () +"/myPDFFile.pdf";
+        File myFile= new File ( myFilePath );
+        try {
+            myPdfDocument.writeTo ( new FileOutputStream ( myFile ) );
+
+        }
+        catch (Exception e){e.printStackTrace ();
+        editText.setText ( "ERROR" );
+        }
+        myPdfDocument.close ();
+}
 }
